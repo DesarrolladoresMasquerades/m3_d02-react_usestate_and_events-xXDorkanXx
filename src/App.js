5ce7ff55e4c046e19d21957b675f7b4f91760b69
Fer Sánchez <sangarfer@gmail.com>
2022-03-01 11:20:17 +0100

    morning class done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Counter from "./Counter";
+//import Counter from "./Counter";
+import { useState } from "react";
 
 function App() {
+
+  const [theme, setTheme] = useState("dark");
+
+  function toggleTheme(event){
+    setTheme(event.target.value)
+  }
+
   return (
-    <div className={"App"}>
+    <div className={"App " + theme}>
       <h1>React - state and events</h1>
-      <Counter />
+      <select onChange={toggleTheme}>
+        <option value={"light"} selected={theme === "light"}>Light theme</option>
+        <option value={"dark"} selected={theme === "dark"}>Dark theme</option>
+      </select>
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,11 +5,19 @@ export default function Counter(){
     //const [myObject, setMyObject] = useState({drink: "cappuccino"})
     //const [dayOfWeek, setDayOfWeek] = useState("Monday")
     //const [holiday, setHoliday] = useState(true)
+    function addOne(){
+        setCounter(counter + 1)
+    }
+    
+    function deleteOne(){
+        setCounter(counter - 1)
+    }
+
     return(
         <div className="Counter">
             <p>You have a total of: <span>{counter}</span></p>
-            <button onClick={()=>setCounter(counter + 1)}>Add 1</button>
-            <button onClick={()=>setCounter(counter - 1)}>Substract 1</button>
+            <button onClick={addOne}>Add 1</button>
+            <button onClick={deleteOne}>Substract 1</button>
         </div>
     )
 }
